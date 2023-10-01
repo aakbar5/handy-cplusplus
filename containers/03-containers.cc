dff519d726ea780a26b59d9f705ce2c23ead427f
aakbar5 <16612387+aakbar5@users.noreply.github.com>
2023-10-01 14:26:02 +0100

    Revise priority_queue implementation to cover more use-cases

--- containers/03-containers.cc
+++ containers/03-containers.cc
@@ -1,6 +1,9 @@
 //
 // Program
 //  Containers: std::priority_queue usage
+//   - Simple priority_queue
+//   - priority_queue with custom compare
+//   - priority_queue for a custom object
 //
 // Compile
 //  g++ -Wall -Wextra -pedantic -std=c++17 -o 03-containers 03-containers.cc
@@ -12,6 +15,7 @@
 #include <iostream>
 #include <queue>
 #include <functional>
+#include <cassert>
 
 //
 // Entry function
@@ -20,42 +24,89 @@ int main() {
   // Simple priority_queue
   std::cout << "std::priority_queue ---" << '\n';
   {
-    std::priority_queue<int> qu;
-    std::cout << "Is empty: " << std::boolalpha << qu.empty() << '\n';
-    std::cout << "Size: " << qu.size() << '\n';
+    std::priority_queue<int> pq;
+    std::cout << "Is empty: " << std::boolalpha << pq.empty() << '\n';
+    std::cout << "Size: " << pq.size() << '\n';
+
+    assert(true == pq.empty());
+    assert(0 == pq.size());
 
     // push elements now
-    qu.push(50);
-    qu.push(45);
-    qu.push(65);
-    qu.push(25);
-    qu.push(10);
-    qu.push(5);
+    pq.push(50);
+    pq.push(45);
+    pq.push(65);
+    pq.push(25);
+    pq.push(10);
+    pq.push(5);
 
-    std::cout << "Is empty: " << std::boolalpha << qu.empty() << '\n';
-    std::cout << "Size: " << qu.size() << '\n';
-    std::cout << "Top element: " << qu.top() << '\n';
+    std::cout << "Is empty: " << std::boolalpha << pq.empty() << '\n';
+    std::cout << "Size: " << pq.size() << '\n';
+    std::cout << "Top element: " << pq.top() << '\n';
+
+    assert(false == pq.empty());
+    assert(6 == pq.size());
+    assert(65 == pq.top());
   }
 
   // priority_queue with custom compare
   std::cout << "std::priority_queue with custom compare ---" << '\n';
   {
-    std::priority_queue<int, std::vector<int>, std::greater<int>> qu;
+    std::priority_queue<int, std::vector<int>, std::greater<int>> pq;
 
-    std::cout << "Is empty: " << std::boolalpha << qu.empty() << '\n';
-    std::cout << "Size: " << qu.size() << '\n';
+    std::cout << "Is empty: " << std::boolalpha << pq.empty() << '\n';
+    std::cout << "Size: " << pq.size() << '\n';
+
+    assert(true == pq.empty());
+    assert(0 == pq.size());
 
     // push elements now
-    qu.push(50);
-    qu.push(45);
-    qu.push(65);
-    qu.push(25);
-    qu.push(10);
-    qu.push(5);
+    pq.push(50);
+    pq.push(45);
+    pq.push(65);
+    pq.push(25);
+    pq.push(10);
+    pq.push(5);
 
-    std::cout << "Is empty: " << std::boolalpha << qu.empty() << '\n';
-    std::cout << "Size: " << qu.size() << '\n';
-    std::cout << "Top element: " << qu.top() << '\n';
+    std::cout << "Is empty: " << std::boolalpha << pq.empty() << '\n';
+    std::cout << "Size: " << pq.size() << '\n';
+    std::cout << "Top element: " << pq.top() << '\n';
+
+    assert(false == pq.empty());
+    assert(6 == pq.size());
+  }
+
+  // priority_queue for a custom object
+  std::cout << "std::priority_queue for a custom object ---" << '\n';
+  {
+    struct pack {
+      std::string name;
+      uint32_t val;
+    };
+
+    struct Comparator {
+      bool operator () (const pack &lhs, const pack &rhs) {
+        return (lhs.val < rhs.val);
+      }
+    };
+
+    // Define custom type of priority_queue
+    using priority_queue = std::priority_queue<pack, std::vector<pack>, Comparator>;
+
+    // Push elements
+    priority_queue pq;
+    pq.push({"value2", 2});
+    pq.push({"value3", 3});
+    pq.push({"value4", 4});
+    pq.push({"value1", 1});
+    pq.push({"value0", 0});
+
+    std::cout << "Is empty: " << std::boolalpha << pq.empty() << '\n';
+    std::cout << "Size: " << pq.size() << '\n';
+    std::cout << "Top element: " << "{" << pq.top().name << "," << pq.top().val << "}" << '\n';
+
+    assert(false == pq.empty());
+    assert(5 == pq.size());
+    assert(4 == pq.top().val);
   }
 
   return 0;
